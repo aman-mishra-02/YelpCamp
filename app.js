4c3933dff13da2c621e8f0f8ed34752bcf4e60a5
Aman Mishra <amthgr0209@gmail.com>
2021-12-11 01:36:00 +0530

    Register Form Added

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 const ExpressError = require("./Utilities/ExpressError");
 const methodOverride = require("method-override");
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
+const userRoutes = require('./routes/users')
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStratagy = require('passport-local');
+const User = require('./models/user');
 
 mongoose.connect("mongodb://localhost:27017/YelpCamp");
 
@@ -40,14 +44,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStratagy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
-});
+}); 
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
